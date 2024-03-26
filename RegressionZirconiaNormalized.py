44762ad1b8c6ddc6fb53cd865e8d9c93f9926e44
Dimitris Lezos <d.lezos@acg.edu>
2024-03-26 16:28:39 +0200

    More tests

--- RegressionZirconiaNormalized.py
+++ RegressionZirconiaNormalized.py
@@ -57,16 +57,25 @@ data[['cut', 'color', 'clarity']] = encoder.fit_transform(cuts)
 #### 3.5 Remove the id column
 data.drop(columns=['id'], inplace=True)
 #### 4. Normalize the data in the numerical columns?
-# price = data[['price']]
-# data = pd.DataFrame(MinMaxScaler().fit_transform(data.iloc[:,:-1]), columns=data.columns[:-1])
-# data = data.join(price)
-# try:
-#     data.drop(columns=['id'], inplace=True)
-# except:
-#     pass
+price = data[['price']]
+data = pd.DataFrame(MinMaxScaler().fit_transform(data.iloc[:,:-1]), columns=data.columns[:-1])
+data = data.join(price)
+try:
+    data.drop(columns=['id'], inplace=True)
+except:
+    pass
 #### 4. Split between training and test data
 # Add price / carat
 data['price_carat'] = data['price'] / data['carat']
+
+print('Full Data: ', data.shape)
+data = data[data['carat'] <= 0.54]
+data = data[0.39 <= data['depth']]
+data = data[data['depth'] <= 0.62]
+data = data[0.135 <= data['table']]
+data = data[data['table'] <= 0.41]
+print('Non Ouliers Data: ', data.shape)
+
 x_train, x_test, y_train, y_test = train_test_split (data.iloc[:,:-2],data.iloc[:,-2:], test_size=0.3, random_state=42)
 #x_train, x_test, y_train, y_test = train_test_split (data.iloc[:,:-2],data.iloc[:,-2:], test_size=0.3, random_state=23)
 # Verify that the split gives similar distributions between train and test on the Ordinal columns
@@ -76,51 +85,51 @@ try:
     print(x_train['clarity'].value_counts(normalize=True), x_test['clarity'].value_counts(normalize=True))
 except:
     pass
-# Also show in plots
-plt.figure(1)
-plt.plot(x_train['cut'].value_counts(normalize=True, sort=False).sort_index()+0.02)
-plt.plot(x_test['cut'].value_counts(normalize=True, sort=False).sort_index())
-plt.figure(2)
-plt.plot(x_train['color'].value_counts(normalize=True, sort=False).sort_index()+0.02)
-plt.plot(x_test['color'].value_counts(normalize=True, sort=False).sort_index())
-plt.figure(3)
-plt.plot(x_train['clarity'].value_counts(normalize=True, sort=False).sort_index()+0.02)
-plt.plot(x_test['clarity'].value_counts(normalize=True, sort=False).sort_index())
-plt.show()
-#### 5. Investigate the correlation of the data
-print(data.describe())
-corr_matrix = data.corr()
-f, ax = plt.subplots(figsize=(14, 8))
-sns.heatmap(corr_matrix,
-            annot=True,
-            annot_kws={'size': 8},
-            cmap="Spectral_r")
-plt.show()
+# # Also show in plots
+# plt.figure(1)
+# plt.plot(x_train['cut'].value_counts(normalize=True, sort=False).sort_index()+0.02)
+# plt.plot(x_test['cut'].value_counts(normalize=True, sort=False).sort_index())
+# plt.figure(2)
+# plt.plot(x_train['color'].value_counts(normalize=True, sort=False).sort_index()+0.02)
+# plt.plot(x_test['color'].value_counts(normalize=True, sort=False).sort_index())
+# plt.figure(3)
+# plt.plot(x_train['clarity'].value_counts(normalize=True, sort=False).sort_index()+0.02)
+# plt.plot(x_test['clarity'].value_counts(normalize=True, sort=False).sort_index())
+# plt.show()
+# #### 5. Investigate the correlation of the data
+# print(data.describe())
+# corr_matrix = data.corr()
+# f, ax = plt.subplots(figsize=(14, 8))
+# sns.heatmap(corr_matrix,
+#             annot=True,
+#             annot_kws={'size': 8},
+#             cmap="Spectral_r")
+# plt.show()
 # We see that carat, x, y, z are completely correlated, so we remove x, y & z features
 data.drop(columns=['x', 'y', 'z'], inplace=True)
 x_train.drop(columns=['x', 'y', 'z'], inplace=True)
 x_test.drop(columns=['x', 'y', 'z'], inplace=True)
-# Let's see the correlation again
-corr_matrix = data.corr()
-f, ax = plt.subplots(figsize=(14, 8))
-sns.heatmap(corr_matrix,
-            annot=True,
-            annot_kws={'size': 8},
-            cmap="Spectral_r")
-plt.show()
+# # Let's see the correlation again
+# corr_matrix = data.corr()
+# f, ax = plt.subplots(figsize=(14, 8))
+# sns.heatmap(corr_matrix,
+#             annot=True,
+#             annot_kws={'size': 8},
+#             cmap="Spectral_r")
+# plt.show()
 # Price and carat are very closely correlated as expected we will try to train for two cases:
 # a. Price
 # b. Price / carat
-#### 5.5 Boxplots
-plt.figure(4)
-plot = sns.boxplot(data=data.drop(columns=['price', 'price_carat']))
-plt.figure(41)
-scaler = MinMaxScaler()
-box_data = data.drop(columns=['price', 'price_carat'])
-box_data = pd.DataFrame(scaler.fit_transform(box_data), index = box_data.index, columns = box_data.columns)
-plot = sns.boxplot(data=box_data)
-plt.show()
-exit(0)
+# #### 5.5 Boxplots
+# plt.figure(4)
+# plot = sns.boxplot(data=data.drop(columns=['price', 'price_carat']))
+# plt.figure(41)
+# scaler = MinMaxScaler()
+# box_data = data.drop(columns=['price', 'price_carat'])
+# box_data = pd.DataFrame(scaler.fit_transform(box_data), index = box_data.index, columns = box_data.columns)
+# plot = sns.boxplot(data=box_data)
+# plt.show()
+#exit(0)
 
 #### 6. Do a simple linear regression for each column
 for col in x_train.columns:
@@ -147,55 +156,55 @@ mse = mean_squared_error(y_test['price'], y_pred)
 print('------------------------')
 print('All columns', 'linear MSE=', round(mse, 2))
 print('All columns', 'linear R2=', r2_score(y_test['price'], y_pred))
-# Plot outputs
-plt.figure(1)
-plt.title('Linear Regression')
-plt.ylabel('yyy')
-plt.xlabel('Input feature=carat')
-plt.scatter(x_test[:100]['carat'], y_test[:100]['price'],  color='black')
-plt.scatter(x_test[:100]['carat'], y_pred[:100], color='blue')
-plt.xticks(())
-plt.yticks(())
-plt.show()
+# # Plot outputs
+# plt.figure(1)
+# plt.title('Linear Regression')
+# plt.ylabel('yyy')
+# plt.xlabel('Input feature=carat')
+# plt.scatter(x_test[:100]['carat'], y_test[:100]['price'],  color='black')
+# plt.scatter(x_test[:100]['carat'], y_pred[:100], color='blue')
+# plt.xticks(())
+# plt.yticks(())
+# plt.show()
 # Try PCA (NOPE!)
-pca = PCA()
-fit_data = pd.DataFrame(pca.fit_transform(data.iloc[:,1:-1]))
-print(pca.explained_variance_ratio_)
-x_train, x_test, y_train, y_test = train_test_split (fit_data,data.iloc[:,-1:], test_size=0.3, random_state=42)
-# Create the model
-linear_regression = LinearRegression()
-# training the linear model
-linear_regression.fit(x_train, y_train)
-# Predict
-y_pred = linear_regression.predict(x_test)
-# calculating mse
-mse = np.mean((y_pred - y_test) ** 2)
-mse = mean_squared_error(y_test, y_pred)
-print('------------------------')
-print('All columns PCA', 'linear MSE=', round(mse, 2))
-print('All columns PCA', 'linear R2=', r2_score(y_test, y_pred))
+# pca = PCA()
+# fit_data = pd.DataFrame(pca.fit_transform(data.iloc[:,1:-1]))
+# print(pca.explained_variance_ratio_)
+# x_train, x_test, y_train, y_test = train_test_split (fit_data,data.iloc[:,-1:], test_size=0.3, random_state=42)
+# # Create the model
+# linear_regression = LinearRegression()
+# # training the linear model
+# linear_regression.fit(x_train, y_train)
+# # Predict
+# y_pred = linear_regression.predict(x_test)
+# # calculating mse
+# mse = np.mean((y_pred - y_test) ** 2)
+# mse = mean_squared_error(y_test, y_pred)
+# print('------------------------')
+# print('All columns PCA', 'linear MSE=', round(mse, 2))
+# print('All columns PCA', 'linear R2=', r2_score(y_test, y_pred))
 #### 7. Do Polynomial linear regression for each column
-for col in x_train.columns:
-    # Create the model
-    linear_regression = LinearRegression()
-
-    poly = PolynomialFeatures(degree=2)
-    # features for poly regression
-    #x_poly = poly.fit_transform(x_train[[col]])
-    # training the linear model
-    linear_regression.fit(poly.fit_transform(x_train[[col]]), y_train)
-    # Predict
-    y_pred = linear_regression.predict(poly.fit_transform(x_test[[col]]))
-    # calculating mse
-    mse = np.mean((y_pred - y_test) ** 2)
-    mse = mean_squared_error(y_test, y_pred)
-    print('------------------------')
-    print(col, 'polynomial MSE=', round(mse, 2))
-    print(col, 'polynomial R2=', r2_score(y_test, y_pred))
+# for col in x_train.columns:
+#     # Create the model
+#     linear_regression = LinearRegression()
+#
+#     poly = PolynomialFeatures(degree=2)
+#     # features for poly regression
+#     #x_poly = poly.fit_transform(x_train[[col]])
+#     # training the linear model
+#     linear_regression.fit(poly.fit_transform(x_train[[col]]), y_train)
+#     # Predict
+#     y_pred = linear_regression.predict(poly.fit_transform(x_test[[col]]))
+#     # calculating mse
+#     mse = np.mean((y_pred - y_test) ** 2)
+#     mse = mean_squared_error(y_test, y_pred)
+#     print('------------------------')
+#     print(col, 'polynomial MSE=', round(mse, 2))
+#     print(col, 'polynomial R2=', r2_score(y_test, y_pred))
 print('# Polynomial regression: ###################')
 #degrees = range(2,7)
-degrees = range(5,6)
-#degrees = range(2,3)
+#degrees = range(7,9)
+degrees = range(2,3)
 for degree in degrees:
     # For all columns
     # Create the model
@@ -212,63 +221,63 @@ for degree in degrees:
     print('All columns', 'polynomial', degree, 'R2=', r2_score(y_test['price'], y_pred))
 #########################
 #### 6. Do a simple linear regression for each column but for price/carat
-for col in x_train.columns:
-    # Create the model
-    linear_regression = LinearRegression()
-    # training the linear model
-    linear_regression.fit(x_train[[col]], y_train['price_carat'])
-    # Predict
-    y_pred = linear_regression.predict(x_test[[col]])
-    # calculating mse
-    mse = mean_squared_error(y_test['price_carat'], y_pred)
-    print('------------------------')
-    print(col, ' price/carat linear MSE=', round(mse, 2))
-    print(col, ' price/carat linear R2=', r2_score(y_test['price_carat'], y_pred))
-# For all columns
-# Create the model
-linear_regression = LinearRegression()
-# training the linear model
-linear_regression.fit(x_train.drop(columns=['carat']), y_train['price_carat'])
-# Predict
-y_pred = linear_regression.predict(x_test.drop(columns=['carat']))
-# calculating mse
-mse = mean_squared_error(y_test['price_carat'], y_pred)
-print('------------------------')
-print('All columns price/carat', 'linear MSE=', round(mse, 2))
-print('All columns price/carat', 'linear R2=', r2_score(y_test['price_carat'], y_pred))
+# for col in x_train.columns:
+#     # Create the model
+#     linear_regression = LinearRegression()
+#     # training the linear model
+#     linear_regression.fit(x_train[[col]], y_train['price_carat'])
+#     # Predict
+#     y_pred = linear_regression.predict(x_test[[col]])
+#     # calculating mse
+#     mse = mean_squared_error(y_test['price_carat'], y_pred)
+#     print('------------------------')
+#     print(col, ' price/carat linear MSE=', round(mse, 2))
+#     print(col, ' price/carat linear R2=', r2_score(y_test['price_carat'], y_pred))
+# # For all columns
+# # Create the model
+# linear_regression = LinearRegression()
+# # training the linear model
+# linear_regression.fit(x_train.drop(columns=['carat']), y_train['price_carat'])
+# # Predict
+# y_pred = linear_regression.predict(x_test.drop(columns=['carat']))
+# # calculating mse
+# mse = mean_squared_error(y_test['price_carat'], y_pred)
+# print('------------------------')
+# print('All columns price/carat', 'linear MSE=', round(mse, 2))
+# print('All columns price/carat', 'linear R2=', r2_score(y_test['price_carat'], y_pred))
 #### 7. Do Polynomial linear regression for each column but for price/carat
-for col in x_train.columns:
-    # Create the model
-    linear_regression = LinearRegression()
-
-    poly = PolynomialFeatures(degree=2)
-    # features for poly regression
-    #x_poly = poly.fit_transform(x_train[[col]])
-    # training the linear model
-    linear_regression.fit(poly.fit_transform(x_train[[col]]), y_train['price_carat'])
-    # Predict
-    y_pred = linear_regression.predict(poly.fit_transform(x_test[[col]]))
-    # calculating mse
-    mse = np.mean((y_pred - y_test['price_carat']) ** 2)
-    mse = mean_squared_error(y_test['price_carat'], y_pred)
-    print('------------------------')
-    print(col, 'polynomial price_carat MSE=', round(mse, 2))
-    print(col, 'polynomial price_carat R2=', r2_score(y_test['price_carat'], y_pred))
-print('# Polynomial regression: ###################')
-for degree in degrees:
-    # For all columns
-    # Create the model
-    linear_regression = LinearRegression()
-    poly = PolynomialFeatures(degree=degree)
-    # training the linear model
-    linear_regression.fit(poly.fit_transform(x_train.drop(columns=['carat'])), y_train['price_carat'])
-    # Predict
-    y_pred = linear_regression.predict(poly.fit_transform(x_test.drop(columns=['carat'])))
-    # calculating mse
-    mse = mean_squared_error(y_test['price'], y_pred)
-    print('------------------------')
-    print('All columns price/carat', 'polynomial', degree, 'MSE=', round(mse, 2))
-    print('All columns price/carat', 'polynomial', degree, 'R2=', r2_score(y_test['price_carat'], y_pred))
+# for col in x_train.columns:
+#     # Create the model
+#     linear_regression = LinearRegression()
+#
+#     poly = PolynomialFeatures(degree=2)
+#     # features for poly regression
+#     #x_poly = poly.fit_transform(x_train[[col]])
+#     # training the linear model
+#     linear_regression.fit(poly.fit_transform(x_train[[col]]), y_train['price_carat'])
+#     # Predict
+#     y_pred = linear_regression.predict(poly.fit_transform(x_test[[col]]))
+#     # calculating mse
+#     mse = np.mean((y_pred - y_test['price_carat']) ** 2)
+#     mse = mean_squared_error(y_test['price_carat'], y_pred)
+#     print('------------------------')
+#     print(col, 'polynomial price_carat MSE=', round(mse, 2))
+#     print(col, 'polynomial price_carat R2=', r2_score(y_test['price_carat'], y_pred))
+# print('# Polynomial regression: ###################')
+# for degree in degrees:
+#     # For all columns
+#     # Create the model
+#     linear_regression = LinearRegression()
+#     poly = PolynomialFeatures(degree=degree)
+#     # training the linear model
+#     linear_regression.fit(poly.fit_transform(x_train.drop(columns=['carat'])), y_train['price_carat'])
+#     # Predict
+#     y_pred = linear_regression.predict(poly.fit_transform(x_test.drop(columns=['carat'])))
+#     # calculating mse
+#     mse = mean_squared_error(y_test['price'], y_pred)
+#     print('------------------------')
+#     print('All columns price/carat', 'polynomial', degree, 'MSE=', round(mse, 2))
+#     print('All columns price/carat', 'polynomial', degree, 'R2=', r2_score(y_test['price_carat'], y_pred))
 #########################################
 #### 6. Do a simple linear regression for columns carat, clarity, table
 # # for col in x_train.columns:
@@ -349,7 +358,7 @@ mse = mean_squared_error(y_test['price'], y_pred)
 print('------------------------')
 print('All columns Lasso MSE=', round(mse, 2))
 print('All columns Lasso R2=', r2_score(y_test['price'], y_pred))
-exit(0)
+#exit(0)
 #### 10 Backward Stepwise Polynomial regression
 # print('# Polynomial regression: ###################')
 x_train_drop = x_train.copy()
@@ -358,7 +367,8 @@ x_test_drop = x_test.copy()
 for column in ['depth', 'cut', 'table', 'clarity', 'color']:
     x_train_drop.drop(columns=[column], inplace=True)
     x_test_drop.drop(columns=[column], inplace=True)
-    for degree in degrees: #range(2,6):
+    for degree in range(8,9):
+#    for degree in degrees: #range(2,6):
         # For all columns
         # Create the model
         linear_regression = LinearRegression()
@@ -373,48 +383,63 @@ for column in ['depth', 'cut', 'table', 'clarity', 'color']:
         print(x_train_drop.columns, 'polynomial', degree, 'MSE=', round(mse, 2))
         print(x_train_drop.columns, 'polynomial', degree, 'R2=', r2_score(y_test['price'], y_pred))
 
-#### 11 NN regression
-from tensorflow.keras import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.losses import MeanSquaredLogarithmicError, MeanSquaredError
-
-
-learning_rate = 0.01
-# Creating model using the Sequential in tensorflow
-model = Sequential([
-    Dense(100, kernel_initializer='normal', activation='relu'),
-    Dropout(0.2),
-    Dense(200, kernel_initializer='normal', activation='relu'),
-    Dropout(0.2),
-    Dense(50, kernel_initializer='normal', activation='relu'),
-    Dense(1, kernel_initializer='normal', activation='linear')
-])
-
-# loss function
-msle = MeanSquaredLogarithmicError()
-mse = MeanSquaredError()
-model.compile(
-    loss=msle,
-    optimizer=Adam(learning_rate=learning_rate),
-    #metrics=[msle],
-    metrics = [mse],
-)
-
-# train the model
-history = model.fit(
-    x_train.values,
-    y_train['price'].values,
-    epochs=10,
-    batch_size=300,
-    validation_split=0.2,
-)
-
-y_pred = model.predict(x_test)
+# #### 11 NN regression
+# from tensorflow.keras import Sequential
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras.layers import Dense, Dropout
+# from tensorflow.keras.losses import MeanSquaredLogarithmicError, MeanSquaredError
+#
+#
+# learning_rate = 0.01
+# # Creating model using the Sequential in tensorflow
+# model = Sequential([
+#     Dense(100, kernel_initializer='normal', activation='relu'),
+#     Dropout(0.2),
+#     Dense(200, kernel_initializer='normal', activation='relu'),
+#     Dropout(0.2),
+#     Dense(50, kernel_initializer='normal', activation='relu'),
+#     Dense(1, kernel_initializer='normal', activation='linear')
+# ])
+#
+# # loss function
+# msle = MeanSquaredLogarithmicError()
+# mse = MeanSquaredError()
+# model.compile(
+#     loss=msle,
+#     optimizer=Adam(learning_rate=learning_rate),
+#     #metrics=[msle],
+#     metrics = [mse],
+# )
+#
+# # train the model
+# history = model.fit(
+#     x_train.values,
+#     y_train['price'].values,
+#     epochs=10,
+#     batch_size=300,
+#     validation_split=0.2,
+# )
+#
+# y_pred = model.predict(x_test)
+# # calculating mse
+# mse = mean_squared_error(y_test['price'], y_pred)
+# print('------------------------')
+# print('NN MSE=', round(mse, 2))
+# print('NN R2=', r2_score(y_test['price'], y_pred))
+#
+# exit(0)
+#######################################################
+# Best of class
+x_train_drop = x_train.copy()
+x_test_drop = x_test.copy()
+x_train_drop.drop(columns=['depth', 'cut'], inplace=True)
+x_test_drop.drop(columns=['depth', 'cut'], inplace=True)
+poly = PolynomialFeatures(degree=8)
+ridge_regression = Ridge(alpha=0.1)
+ridge_regression.fit(poly.fit_transform(x_train_drop), y_train['price'])
+y_pred = ridge_regression.predict(poly.fit_transform(x_test_drop))
 # calculating mse
 mse = mean_squared_error(y_test['price'], y_pred)
 print('------------------------')
-print('NN MSE=', round(mse, 2))
-print('NN R2=', r2_score(y_test['price'], y_pred))
-
-exit(0)
+print("['carat', 'cut', 'color', 'clarity', 'table'] Ridge Poly 8 MSE=", round(mse, 2))
+print("['carat', 'cut', 'color', 'clarity', 'table'] Ridge Poly 8  R2=", r2_score(y_test['price'], y_pred))
